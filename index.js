0b1f002c9483e2cc6a3fd33ab5bed7883ef44b03
Morgan Anders <MorganAndersDP@gmail.com>
2019-08-17 03:44:21 -0700

    Express Router

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -9,6 +10,17 @@ const port = 3000;
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>')
+});
 
 app.all('./dishes', (req, res, next) => {
     res.statusCode = 200;
@@ -50,15 +62,6 @@ app.put('/dishes/:dishID', (req, res, next) => {
 
 app.delete('/dishes/:dishID', (req, res, next) => {
     res.end('Deleting dish: ' + req.params.dishId);
-});
-
-app.use(express.static(__dirname + '/public'));
-
-app.use((req, res, next) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is an Express Server</h1></body></html>')
 });
 
 const server = http.createServer(app);
